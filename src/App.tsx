254b28828c4fddfd5c8e005cdc8d2e78b94c1b33
Dinh The Nhan <nhandtps40210@gmail.com>
2026-01-27 02:21:48 +0700

    on anti devtools

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,14 @@ import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
 import Guide from './pages/Guide';
 import { enableScreenshotProtection } from './utils/screenshotProtection';
-// import { enableDevToolsProtection } from './utils/devToolsProtection';
+import { enableDevToolsProtection } from './utils/devToolsProtection';
 
 function App() {
   // Enable DevTools protection - redirects to Google if DevTools is detected
-  // TẠM THỜI TẮT ĐỂ TEST
-  // useEffect(() => {
-  //   const cleanupDevTools = enableDevToolsProtection();
-  //   return cleanupDevTools;
-  // }, []);
+  useEffect(() => {
+    const cleanupDevTools = enableDevToolsProtection();
+    return cleanupDevTools;
+  }, []);
 
   // Enable screenshot protection globally
   useEffect(() => {
